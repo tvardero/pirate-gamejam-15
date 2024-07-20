dbcd17f2c484bebc15927cc6baff7ca969361931
Kri <kri.boats@gmail.com>
2024-07-20 17:45:04 +0200

    Added SoundPlayer singleton Added past/present music crossfading Added temp music assets

--- scenes/singletons/WorldState.gd
+++ scenes/singletons/WorldState.gd
@@ -3,8 +3,9 @@ extends Node
 var in_future: bool = false;
 
 func use_lantern() -> void:
-    print("todo: lantern, now in future: " + str(in_future))
-    set_time(!in_future)
+	set_time(!in_future)
+	print("todo: lantern, now in future: " + str(in_future))
 
 func set_time(to_future: bool) -> void:
-    in_future = to_future;
+	in_future = to_future;
+	SoundPlayer.set_music_track(in_future, 1)
