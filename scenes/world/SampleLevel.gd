c6e5d49b725b5773a5c68586e03b8e9a7fdb1e9f
Vardo Ternos <tvardero@gmail.com>
2024-07-22 11:37:18 +0300

    Add spawn points

--- scenes/world/SampleLevel.gd
+++ scenes/world/SampleLevel.gd
@@ -1,7 +1,8 @@
-extends Node2D
+extends Level
 
 @export var present_music: AudioStream
 @export var past_music: AudioStream
 
 func _ready() -> void:	
+	super._ready();
 	SoundPlayer.play_music(present_music, past_music)
