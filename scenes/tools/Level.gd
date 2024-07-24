10a9879cd66c1ab9ab82b0c2c8565767d3e8b4d6
Vardo Ternos <tvardero@gmail.com>
2024-07-24 18:01:48 +0300

    Add time-change logic

--- scenes/tools/Level.gd
+++ scenes/tools/Level.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var auto_spawn_player: bool = false
 @export var auto_spawn_id: int = 0;
+@export var auto_spawn_direction: Vector2 = Vector2.ZERO;
 
 @export var present_music: AudioStream
 @export var past_music: AudioStream
@@ -10,7 +11,7 @@ extends Node2D
 var player_packed: PackedScene = preload ("res://scenes/characters/player/Player.tscn");
 
 func _ready():
-	if (auto_spawn_player&&!WorldState.player_exists): spawn_player_at(auto_spawn_id);
+	if (auto_spawn_player&&!WorldState.player_exists): spawn_player_at(auto_spawn_id, auto_spawn_direction);
 	if present_music&&past_music:
 		SoundPlayer.play_music(present_music, past_music)
 
@@ -33,3 +34,6 @@ func find_spawnpoint(spawn_id: int) -> SpawnPoint:
 		if child is SpawnPoint&&child.id == spawn_id: return child;
 
 	return null;
+
+func switch_time(_to_future: bool):
+	print("Switching time is not implemented for this level: " + str(name));
