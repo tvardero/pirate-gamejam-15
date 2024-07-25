7b7e00f5d02b77c4c95358133dbfebfcf46a9002
Kri <kri.boats@gmail.com>
2024-07-25 20:11:38 +0200

    dont look

--- scenes/props/star-fragment/fuse_fragment.gd
+++ scenes/props/star-fragment/fuse_fragment.gd
@@ -16,6 +16,7 @@ func _ready():
 	rest_nodes[0].select()
 
 func _on_area_2d_input_event(_viewport, _event, _shape_idx):
+	print(123)
 	if Input.is_action_just_pressed("click"):
 		selected=true
 		instructions.visible = false
@@ -25,24 +26,29 @@ func _physics_process(delta):
 		global_position = lerp(global_position, get_global_mouse_position(), 25*delta)
 	else:
 		global_position = lerp(global_position, rest_point, 10*delta)
-		
+
 func _input(event):
 	if event is InputEventMouseButton:
+		print(1)
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			print(2)
 			selected = false
 			var shortest_distance = 40
 			for child in rest_nodes:
+				print(3)
 				var distance = global_position.distance_to(child.global_position)
 				if distance < shortest_distance:
+					print(4)
 					child.select()
 					rest_point=child.global_position
 					shortest_distance = distance
 					if child.is_destination: resolve()
 					
 func resolve():
+	print(5)
 	anim.play('play')
 	SoundPlayer.play_sound(build_sfx)
 	await get_tree().create_timer(0.4).timeout
 	SoundPlayer.play_sound(woosh_sfx)
 	await get_tree().create_timer(0.3).timeout
-	queue_free()
+	get_parent().queue_free()
