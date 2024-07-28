51415f7416a36bd81511d68199b15d0f435b3e73
Vardo Ternos <tvardero@gmail.com>
2024-07-28 19:27:20 +0300

    Group properties of Level scene

--- scenes/tools/Level.gd
+++ scenes/tools/Level.gd
@@ -1,16 +1,23 @@
 class_name Level
 extends Node2D
 
+@export_group("Player auto spawn")
 @export var auto_spawn_player: bool = false
 @export var auto_spawn_id: int = 0;
 @export var auto_spawn_direction: Vector2 = Vector2.ZERO;
-@export var future_music: AudioStream
-@export var past_music: AudioStream
-@export var past_nodes: Node2D;
-@export var future_nodes: Node2D;
-var _color_rect: ColorRect;
 
-var player_packed: PackedScene = preload ("res://scenes/characters/player/Player.tscn");
+@export_group("\"Past\" configuration")
+@export var past_nodes: Node2D;
+@export var past_bg_color: Color;
+@export var past_music: AudioStream
+
+@export_group("\"Future\" configuration")
+@export var future_nodes: Node2D;
+@export var future_bg_color: Color;
+@export var future_music: AudioStream
+
+var _color_rect: ColorRect;
+var _player_packed: PackedScene = preload ("res://scenes/characters/player/Player.tscn");
 
 func _ready():
 	if (auto_spawn_player&&!WorldState.player_exists): spawn_player_at(auto_spawn_id, auto_spawn_direction);
@@ -33,7 +40,7 @@ func spawn_player_at(spawn_id: int, direction: Vector2=Vector2.ZERO) -> void:
 	var spawn = find_spawnpoint(spawn_id);
 	var spawn_position: Vector2 = spawn.position if spawn else Vector2.ZERO;
 
-	var player = player_packed.instantiate() as Player;
+	var player = _player_packed.instantiate() as Player;
 	player.direction = direction;
 	player.position = spawn_position;
 
