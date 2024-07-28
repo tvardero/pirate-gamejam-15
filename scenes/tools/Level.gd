6379d134d6687f240af9bf3f8e50d4e1541eed95
Vardo Ternos <tvardero@gmail.com>
2024-07-28 22:09:18 +0300

    Smoooth animation of time change

--- scenes/tools/Level.gd
+++ scenes/tools/Level.gd
@@ -8,33 +8,39 @@ extends Node2D
 
 @export_group("\"Past\" configuration")
 @export var past_nodes: Node2D;
-@export var past_bg_color: Color;
+@export var past_color: Color;
 @export var past_music: AudioStream
 
 @export_group("\"Future\" configuration")
 @export var future_nodes: Node2D;
-@export var future_bg_color: Color;
+@export var future_color: Color;
 @export var future_music: AudioStream
 
-var _color_rect: ColorRect;
 var _player_packed: PackedScene = preload ("res://scenes/characters/player/Player.tscn");
+var bg_color: ColorRect;
 
 func _ready():
-	if (auto_spawn_player&&!WorldState.player_exists): spawn_player_at(auto_spawn_id, auto_spawn_direction);
+	if (auto_spawn_player&&!WorldState.player): spawn_player_at(auto_spawn_id, auto_spawn_direction);
+
 	if future_music&&past_music:
 		SoundPlayer.play_music(future_music, past_music)
 
-	_color_rect = ColorRect.new();
-	_color_rect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	bg_color = _create_color_rect();
+	bg_color.color = future_color if WorldState.in_future else past_color;
+	switch_nodes(WorldState.in_future);
+
+func _create_color_rect() -> ColorRect:
+	var color_rect = ColorRect.new();
+	color_rect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	color_rect.color = Color.TRANSPARENT;
 
 	var canvas = CanvasLayer.new();
-	canvas.layer = -100;
-	canvas.add_child(_color_rect);
+	canvas.layer = -1;
+	canvas.add_child(color_rect);
 	
 	add_child(canvas);
-	move_child(canvas, 0);
-	
-	WorldState.set_time(WorldState.in_future);
+
+	return color_rect;
 
 func spawn_player_at(spawn_id: int, direction: Vector2=Vector2.ZERO) -> void:
 	var spawn = find_spawnpoint(spawn_id);
@@ -43,8 +49,6 @@ func spawn_player_at(spawn_id: int, direction: Vector2=Vector2.ZERO) -> void:
 	var player = _player_packed.instantiate() as Player;
 	player.direction = direction;
 	player.position = spawn_position;
-
-	WorldState.player = player;
 
 	call_deferred("add_child", player);
 
@@ -56,7 +60,12 @@ func find_spawnpoint(spawn_id: int) -> SpawnPoint:
 
 	return null;
 
-func switch_time(to_future: bool):
-	_color_rect.color = Color.BLACK if to_future else Color.WHITE;
+func switch_nodes(to_future: bool):
 	past_nodes.visible = !to_future;
 	future_nodes.visible = to_future;
+
+func modulate_nodes(val: float):
+	val = clamp(val, 0, 1);
+	var color = lerp(Color.TRANSPARENT, Color.WHITE, val);
+	past_nodes.modulate = color;
+	future_nodes.modulate = color;
