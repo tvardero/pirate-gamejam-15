cb7271b274e3317254893e51a3ed2096f1b2e7d7
DiceWithDeath <rosieth@live.co.uk>
2024-07-29 20:54:58 +0100

    Lantern fuse is now transparent + no circles

--- scenes/singletons/drop_zone.gd
+++ scenes/singletons/drop_zone.gd
@@ -3,13 +3,13 @@ extends Marker2D
 @export var is_destination: bool = false
 
 func _draw():
-	draw_circle(Vector2.ZERO, 35, Color.BLANCHED_ALMOND)
+	draw_circle(Vector2.ZERO, 35, Color.TRANSPARENT)
 	
 func select():
 	for child in get_tree().get_nodes_in_group("zone"):
 		child.deselect()
-	modulate=Color.WEB_MAROON
+	modulate=Color.TRANSPARENT
 	
 func deselect():
-	modulate = Color.WHITE
+	modulate = Color.TRANSPARENT
 	
