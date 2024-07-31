eb856c6d604ac01de781aa80f006880b37e44f45
Kri <kri.boats@gmail.com>
2024-07-31 10:30:49 +0200

    Added and implemented new Town1 sprites Rebuilt Town1 level

--- scenes/props/gate/town1_gate/town_1_gate.gd
+++ scenes/props/gate/town1_gate/town_1_gate.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
 @onready var collision_shape_closed = $"Gate Closed/CollisionShape2D"
-@onready var collision_shape_open = $"Gate Open/CollisionShape2D"
 @onready var gate_open = $"Gate Open"
 @onready var gate_closed = $"Gate Closed"
 
 func _ready():
 	if WorldState.town1_gate_open == true:
 		collision_shape_closed.disabled = true
-		collision_shape_open.disabled = false
 		gate_closed.visible = false
 		gate_open.visible = true
 	else:
 		collision_shape_closed.disabled = false
-		collision_shape_open.disabled = true
 		gate_closed.visible = true
 		gate_open.visible = false
 
@@ -23,7 +20,6 @@ func _on_interactable_interacted(initiator):
 			SoundPlayer.play_sound(load('res://assets/sounds/gate/gate.wav'))
 			await get_tree().create_timer(1.0).timeout
 			collision_shape_closed.disabled = true
-			collision_shape_open.disabled = false
 			gate_closed.visible = false
 			gate_open.visible = true
 			WorldState.town1_gate_open = true
