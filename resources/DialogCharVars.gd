e829550ac458ebbfb711d7803c3fe45288c5dbc8
Dominick Sabella <dominick.sabella@gmail.com>
2024-08-07 12:53:34 -0400

    Update prior to merge from migration branch

--- resources/DialogCharVars.gd
+++
@@ -1,4 +0,0 @@
-class_name DialogCharVars
-extends Resource
-
-@export var portrait: PackedScene
--- scenes/ui/DialogBalloon.cs
+++ scenes/ui/DialogBalloon.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using DialogueManagerRuntime;
+using Godot.Collections;
 
 public partial class DialogBalloon : CanvasLayer
 {    
@@ -17,5 +18,28 @@ public partial class DialogBalloon : CanvasLayer
     [Export] private AudioStream default_talk_sound;
 
     private Portrait portrait;
+
+    // public override void _Ready()
+    // {
+    //     balloon.Hide();
+    //     DialogueManager.Mutated += DialogueManager_Mutated;
+        
+    //     // If the responses menu doesn't have a next action set, use this one
+    //     if (responsesMenu.Get("next_action").ToString() == string.Empty)
+    //     {
+    //         responsesMenu.Set("next_action", next_action);
+    //     }
+    // }
+
+    // private void DialogueManager_Mutated(Dictionary mutation)
+    // {
+	// is_waiting_for_input = false
+	// will_hide_balloon = true
+	// get_tree().create_timer(0.1).timeout.connect(func():
+	// 	if will_hide_balloon:
+	// 		will_hide_balloon = false
+	// 		balloon.hide()
+	// )
+    // }
 
 }
