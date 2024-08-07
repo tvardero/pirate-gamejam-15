8de5dfcf84a0d99572fbe129288af27f8ec1c737
Vardo Ternos <tvardero@gmail.com>
2024-08-07 16:02:55 +0300

    Rename constants class

---
+++ Code/InputActionNames.cs
@@ -0,0 +1,9 @@
+﻿public static class InputActionNames
+{
+    public const string MOVE_UP = "move_up";
+    public const string MOVE_DOWN = "move_down";
+    public const string MOVE_LEFT = "move_left";
+    public const string MOVE_RIGHT = "move_right";
+    public const string UI_ACCEPT = "ui_accept";
+    public const string UI_CANCEL = "ui_cancel";
+}
